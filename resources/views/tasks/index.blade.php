b86ae84726a2e0ba9bd3e2ed6565fd2eb962d8de
Alejandro Flores <alejandro@esporaestudio.com>
2021-03-12 21:01:15 -0600

    Se agrega la vista inicial de la tarea

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.main')
 
 @section('content')
-<h1>Lista de tareas</h1>
+<div class="row">
+    <div class="col-12">
+        <h1>Lista de tareas</h1>
+    </div>
+</div>
 @endsection
 
