2162e5c56812f8c7ac58b2db87f8bca10a6b86ea
Alejandro Flores <alejandro@esporaestudio.com>
2021-03-12 21:03:17 -0600

    Se agrega el botón de creación de una tarea

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -6,5 +6,12 @@
         <h1>Lista de tareas</h1>
     </div>
 </div>
+<div class="row">
+    <div class="col-12">
+        <a class="btn btn-default" href="{{ route('tasks.create')}}">
+            Crea una tarea
+        </a>
+    </div>
+</div>
 @endsection
 
